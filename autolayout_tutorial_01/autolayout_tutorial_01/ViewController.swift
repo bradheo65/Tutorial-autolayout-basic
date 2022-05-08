9a59965c6b695dbe583382b07fc8992d464cb62c
gjrjs31@naver.com <gjrjs31@naver.com>
2022-05-08 22:11:02 +0900

    chore: autoLayout01 Test
    constant 실습

---
+++ autolayout_tutorial_01/autolayout_tutorial_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autolayout_tutorial_01
+//
+//  Created by 허건 on 2022/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
